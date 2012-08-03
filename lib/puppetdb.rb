1e500fa059d017c3401415fae963636a113fa988
Dan Bode <dan@puppetlabs.com>
2012-08-02 21:34:21 -0700

    Add ability to query all nodes
    This commit adds the capability to query for a list of all nodes.
    
    Ensures that an empty query is the default for the --query option of
    the node query subcommand.
    
    Adds functionality so that if a query is empty, it will call the nodes
    url (getting all nodes)

--- lib/puppetdb.rb
+++ lib/puppetdb.rb
@@ -11,6 +11,8 @@ class PuppetDB
 
     truth_values = []
 
+    return query_puppetdb(host, port, :empty) if stack == []
+
     stack.each do |exp|
       case exp.keys.first
         when "statement"
@@ -47,18 +49,26 @@ class PuppetDB
 
   def query_puppetdb(host, port, query)
     http = Net::HTTP.new(host, port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    #http.use_ssl = true
+    #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    type = query.keys.first
+    if query == :empty
 
-    case type
-      when "resources"
-        resp, data = http.get("/resources?query=%s" % URI.escape(query[type].to_json), {"accept" => "application/json"})
-        return JSON.parse(data).map{|f| f["certname"]}
-      when "nodes"
-        resp, data = http.get("/nodes?query=%s" % URI.escape(query[type].to_json), {"accept" => "application/json"})
-        return JSON.parse(data)
+      resp, data = http.get("/nodes", {"accept" => "application/json"})
+      return JSON.parse(data)
+
+    else
+
+      type = query.keys.first
+
+      case type
+        when "resources"
+          resp, data = http.get("/resources?query=%s" % URI.escape(query[type].to_json), {"accept" => "application/json"})
+          return JSON.parse(data).map{|f| f["certname"]}
+        when "nodes"
+          resp, data = http.get("/nodes?query=%s" % URI.escape(query[type].to_json), {"accept" => "application/json"})
+          return JSON.parse(data)
+      end
     end
   end
 end
