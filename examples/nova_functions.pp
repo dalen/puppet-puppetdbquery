66e2fd74c7a7b35e7f9c56c9f4ebb6bc43d406a4
Erik Dalén <dalen@spotify.com>
2013-03-12 09:46:06 +0100

    Fix some spelling

--- examples/nova_functions.pp
+++ examples/nova_functions.pp
@@ -10,7 +10,7 @@ notice("rabbit password: ${rabbit_connection_hash[password]}")
 
 notice(collect_nova_db_connection('fqdn', 'architecture=amd64'))
 
-$vnc_proxy_host   = unique(query_nodes('Class[Nova::vncproxy]', 'fqdn'))
+$vnc_proxy_host   = unique(query_nodes('Class[Nova::Vncproxy]', 'fqdn'))
 notice("vnc proxy host ${vnc_proxy_host}")
 
 # glance api servers
