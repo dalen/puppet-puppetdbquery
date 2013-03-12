718464c74361702e9422b1f56514748b3bfbaff9
Erik Dalén <dalen@spotify.com>
2013-03-12 09:44:09 +0100

    query_nodes: Add ability to fetch a fact instead of certname
    second argument specifies the fact you want to return.

--- lib/puppet/parser/functions/query_nodes.rb
+++ lib/puppet/parser/functions/query_nodes.rb
@@ -1,24 +1,27 @@
-Puppet::Parser::Functions.newfunction(:query_nodes, :type => :rvalue, :arity => 1, :doc => <<-EOT
+Puppet::Parser::Functions.newfunction(:query_nodes, :type => :rvalue, :arity => -2, :doc => <<-EOT
 
-  accepts two arguments, a query used to discover nodes, and a filter used to
-  specify the fact that should be returned.
+  accepts two arguments, a query used to discover nodes, and a optional
+  fact that should be returned.
 
   The query specified should conform to the following format:
     (Type[title] and fact_name<operator>fact_value) or ...
     Package["mysql-server"] and cluster_id=my_first_cluster
 
-  The filter provided should be a single fact (this argument is optional)
+  The second argument should be single fact (this argument is optional)
 
 EOT
 ) do |args|
-  query = args
+  query, fact = args
 
   require 'puppet/util/puppetdb'
   require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppetdb/connection'))
 
   puppetdb = PuppetDB::Connection.new(Puppet::Util::Puppetdb.server, Puppet::Util::Puppetdb.port)
-  if query.is_a? String then
-    query = puppetdb.parse_query query
+  if fact then
+    query = puppetdb.parse_query query, :facts if query.is_a? String
+    puppetdb.facts([fact], query).each_value.to_a
+  else
+    query = puppetdb.parse_query query if query.is_a? String
+    puppetdb.query :nodes, query
   end
-  puppetdb.query(:nodes, query)
 end
