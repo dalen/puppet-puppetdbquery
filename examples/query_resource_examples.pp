467cfcd3213f7a6a2d469faece96df42c16f5a0d
Erik Dalén <dalen@spotify.com>
2013-04-11 11:46:38 +0200

    remove trailing whitespace in examples file

--- examples/query_resource_examples.pp
+++ examples/query_resource_examples.pp
@@ -30,12 +30,12 @@ $rabbit_password  = $rabbit_params['password']
 notice("rabbit password: ${rabbit_password}")
 
 # return the fqdn of the host that has applied
-$vnc_proxy_host   = unique(query_nodes('Class[Nova::Vncproxy]', 'fqdn')) 
+$vnc_proxy_host   = unique(query_nodes('Class[Nova::Vncproxy]', 'fqdn'))
 notice("vnc proxy host ${vnc_proxy_host}")
 # check the size of this thing and make some decisions
 
 # figure out sql connection
-$nova_db_host      = unique(query_nodes('Class[Nova::Db::Mysql]', 'fqdn')) 
+$nova_db_host      = unique(query_nodes('Class[Nova::Db::Mysql]', 'fqdn'))
 $nova_db_resources = query_resource('Class[Nova::Db::Mysql]', 'architecture=amd64')
 $nova_db_class     = $nova_db_resources['Class[Nova::Db::Mysql]']
 $nova_db_params    = $nova_db_class['parameters']
