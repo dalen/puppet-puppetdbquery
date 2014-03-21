164fd2b950cde43a0a21c733defc0b30ca1f8017
Adrian Lopez <adrianlzt@gmail.com>
2014-03-21 20:10:47 +0100

    Add a --no_ssl option

--- lib/puppet/application/query.rb
+++ lib/puppet/application/query.rb
@@ -7,16 +7,20 @@ class Puppet::Application::Query < Puppet::Application::FaceBase
       require 'puppet/util/puppetdb'
       host = Puppet::Util::Puppetdb.server || 'puppetdb'
       port = Puppet::Util::Puppetdb.port || 8081
+      ssl = false
     rescue Exception => e
       Puppet.debug(e.message)
       host = 'puppetdb'
       port = 8081
+      ssl = false
     end
 
     Puppet.debug(host)
     Puppet.debug(port)
+    Puppet.debug(ssl)
 
     { :host => host,
-      :port => port }
+      :port => port,
+      :ssl => ssl }
   end
 end
