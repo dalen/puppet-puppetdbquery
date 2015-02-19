153281324fbebce681f0c1130dd542cfd59d5156
Nate Brown <nbrown.us@gmail.com>
2015-02-18 18:49:36 -0800

    Adding a group by hosts option for query_resources

--- lib/puppet/parser/functions/query_resources.rb
+++ lib/puppet/parser/functions/query_resources.rb
@@ -1,12 +1,14 @@
-Puppet::Parser::Functions.newfunction(:query_resources, :type => :rvalue, :arity => 2, :doc => <<-EOT
+Puppet::Parser::Functions.newfunction(:query_resources, :type => :rvalue, :arity => -3, :doc => <<-EOT
 
-  Accepts two arguments: a query used to discover nodes, and a
+  Accepts two or three arguments: a query used to discover nodes, a
   resource query for the resources that should be returned from
-  those hosts.
+  those hosts, and optionally a boolean for whether or not to group the results by host.
 
-  The result is a hash that maps the name of the nodes to a list of
+  The result is a hash (by default) that maps the name of the nodes to a list of
   resource entries.  This is a list because there's no single
   reliable key for resource operations that's of any use to the end user.
+
+  If the third parameters is false the result will be a an array of all resources found.
 
   Examples:
 
@@ -22,9 +24,13 @@ Puppet::Parser::Functions.newfunction(:query_resources, :type => :rvalue, :arity
 
       query_resources(false, 'Class["apache"]')
 
+    Returns the parameters for the apache class for all nodes in a flat array:
+
+      query_resources(false, 'Class["apache"]', false)
+
 EOT
 ) do |args|
-  nodequery, resquery = args
+  nodequery, resquery, grouphosts = args
 
   require 'puppet/util/puppetdb'
   # This is needed if the puppetdb library isn't pluginsynced to the master
@@ -38,5 +44,5 @@ EOT
   puppetdb = PuppetDB::Connection.new(Puppet::Util::Puppetdb.server, Puppet::Util::Puppetdb.port)
   nodequery = puppetdb.parse_query nodequery, :facts if nodequery and nodequery.is_a? String and ! nodequery.empty?
   resquery = puppetdb.parse_query resquery, :none if resquery and resquery.is_a? String and ! resquery.empty?
-  return puppetdb.resources(nodequery, resquery)
+  return puppetdb.resources(nodequery, resquery, nil, grouphosts)
 end
