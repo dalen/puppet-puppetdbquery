8fa9654ade7c945c8946f631800051776178fe55
Erik Dalén <dalen@spotify.com>
2015-09-28 09:51:07 +0200

    Change from connection to parser in spec test

--- spec/unit/puppetdb/parser_spec.rb
+++ spec/unit/puppetdb/parser_spec.rb
@@ -5,59 +5,59 @@ require 'puppetdb/parser'
 
 describe PuppetDB::Parser do
   context "Query parsing" do
-    let(:connection) { PuppetDB::Parser.new }
+    let(:parser) { PuppetDB::Parser.new }
     it "should handle empty queries" do
-      connection.parse('').should be_nil
+      parser.parse('').should be_nil
     end
 
     it "should handle double quoted strings" do
-      connection.parse('foo="bar"').should eq ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "foo"], ["=", "value", "bar"]]]]]
+      parser.parse('foo="bar"').should eq ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "foo"], ["=", "value", "bar"]]]]]
     end
 
     it "should handle single quoted strings" do
-      connection.parse('foo=\'bar\'').should eq ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "foo"], ["=", "value", "bar"]]]]]
+      parser.parse('foo=\'bar\'').should eq ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "foo"], ["=", "value", "bar"]]]]]
     end
 
     it "should handle precedence" do
-      connection.parse('foo=1 or bar=2 and baz=3').should eq ["or", ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "foo"], ["=", "value", 1]]]]], ["and", ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "bar"], ["=", "value", 2]]]]], ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "baz"], ["=", "value", 3]]]]]]]
-      connection.parse('(foo=1 or bar=2) and baz=3').should eq ["and", ["or", ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "foo"], ["=", "value", 1]]]]], ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "bar"], ["=", "value", 2]]]]]], ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "baz"], ["=", "value", 3]]]]]]
+      parser.parse('foo=1 or bar=2 and baz=3').should eq ["or", ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "foo"], ["=", "value", 1]]]]], ["and", ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "bar"], ["=", "value", 2]]]]], ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "baz"], ["=", "value", 3]]]]]]]
+      parser.parse('(foo=1 or bar=2) and baz=3').should eq ["and", ["or", ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "foo"], ["=", "value", 1]]]]], ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "bar"], ["=", "value", 2]]]]]], ["in", "certname", ["extract", "certname", ["select_facts", ["and", ["=", "name", "baz"], ["=", "value", 3]]]]]]
     end
 
     it "should parse resource queries with only type name" do
-      connection.parse('file').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", "type", "File"]]]]]
+      parser.parse('file').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", "type", "File"]]]]]
     end
 
     it "should parse resource queries with only title" do
-      connection.parse('[foo]').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", "title", "foo"]]]]]
+      parser.parse('[foo]').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", "title", "foo"]]]]]
     end
 
     it "should parse resource queries with only parameters" do
-      connection.parse('{foo=bar}').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", ["parameter", "foo"], "bar"]]]]]
+      parser.parse('{foo=bar}').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", ["parameter", "foo"], "bar"]]]]]
     end
 
     it "should parse resource queries for exported resources" do
-      connection.parse('@@file').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", true], ["=", "type", "File"]]]]]
+      parser.parse('@@file').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", true], ["=", "type", "File"]]]]]
     end
 
     it "should parse resource queries with type, title and parameters" do
-      connection.parse('file[foo]{bar=baz}').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", "type", "File"], ["=", "title", "foo"], ["=", ["parameter", "bar"], "baz"]]]]]
+      parser.parse('file[foo]{bar=baz}').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", "type", "File"], ["=", "title", "foo"], ["=", ["parameter", "bar"], "baz"]]]]]
     end
 
     it "should parse resource queries with tags" do
-      connection.parse('file[foo]{tag=baz}').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", "type", "File"], ["=", "title", "foo"], ["=", "tag", "baz"]]]]]
+      parser.parse('file[foo]{tag=baz}').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", "type", "File"], ["=", "title", "foo"], ["=", "tag", "baz"]]]]]
     end
 
     it "should handle precedence within resource parameter queries" do
-      connection.parse('{foo=1 or bar=2 and baz=3}').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["or", ["=", ["parameter", "foo"], 1], ["and", ["=", ["parameter", "bar"], 2], ["=", ["parameter", "baz"], 3]]]]]]]
-      connection.parse('{(foo=1 or bar=2) and baz=3}').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["or", ["=", ["parameter", "foo"], 1], ["=", ["parameter", "bar"], 2]], ["=", ["parameter", "baz"], 3]]]]]
+      parser.parse('{foo=1 or bar=2 and baz=3}').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["or", ["=", ["parameter", "foo"], 1], ["and", ["=", ["parameter", "bar"], 2], ["=", ["parameter", "baz"], 3]]]]]]]
+      parser.parse('{(foo=1 or bar=2) and baz=3}').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["or", ["=", ["parameter", "foo"], 1], ["=", ["parameter", "bar"], 2]], ["=", ["parameter", "baz"], 3]]]]]
     end
 
     it "should capitalize class names" do
-      connection.parse('class[foo::bar]').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", "type", "Class"], ["=", "title", "Foo::Bar"]]]]]
+      parser.parse('class[foo::bar]').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["=", "type", "Class"], ["=", "title", "Foo::Bar"]]]]]
     end
 
     it "should parse resource queries with regeexp title matching" do
-      connection.parse('[~foo]').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["~", "title", "foo"]]]]]
+      parser.parse('[~foo]').should eq ["in", "certname", ["extract", "certname", ["select_resources", ["and", ["=", "exported", false], ["~", "title", "foo"]]]]]
     end
 
     it "should be able to negate expressions" do
