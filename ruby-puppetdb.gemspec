f9ff87fe7b774653508593a1d595b1938c7d5a23
Erik Dalén <dalen@spotify.com>
2015-11-01 19:52:15 +0100

    Add a proper test for query_facts
    Test #71 somewhat properly

--- ruby-puppetdb.gemspec
+++ ruby-puppetdb.gemspec
@@ -26,6 +26,7 @@ Gem::Specification.new do |s|
   s.add_dependency('chronic')
   s.add_development_dependency 'rspec', '2.13'
   s.add_development_dependency 'rspec-expectations'
+  s.add_development_dependency 'rspec-puppet'
   s.add_development_dependency 'rake'
   s.add_development_dependency 'puppetlabs_spec_helper'
   s.add_development_dependency 'puppet'
---
+++ spec/functions/query_facts_spec.rb
@@ -0,0 +1,13 @@
+#! /usr/bin/env ruby -S rspec
+
+require 'spec_helper'
+require 'puppetdb/connection'
+
+describe 'query_facts' do
+  it do
+    PuppetDB::Connection.any_instance.stubs(:query).returns [
+      { 'certname' => 'apache4.puppetexplorer.io', 'environment' => 'production', 'name' => 'ipaddress', 'value' => '172.31.6.80' }
+    ]
+    should run.with_params('', ['ipaddress']).and_return({'apache4.puppetexplorer.io' => {'ipaddress' => '172.31.6.80'}})
+  end
+end
