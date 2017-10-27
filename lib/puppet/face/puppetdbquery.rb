dalen/puppet-puppetdbquery

commit 2c744337432e5601114ad64e59c335ff25b0bc1d

Rename CLI command to puppetdbquery
The "new" PuppetDB bundled query tool clashed with this otherwise.

Fixes: #105

lib/puppet/face/puppetdbquery.rb
@@ -2,13 +2,13 @@ require 'puppet/application/query'
 require 'puppet/face'
 require 'puppet/util/colors'
 
-Puppet::Face.define(:query, '1.0.0') do
+Puppet::Face.define(:puppetdbquery, '1.0.0') do
   require 'puppetdb/connection'
   PuppetDB::Connection.check_version
 
   extend Puppet::Util::Colors
 
-  copyright 'Erik Dalen & Puppet Labs', 2012..2015
+  copyright 'Erik Dalen', 2012..2017
   license 'Apache 2 license; see COPYING'
 
   option '--host PUPPETDB' do

lib/puppet/application/puppetdbquery.rb
@@ -1,6 +1,6 @@
 require 'puppet/application/face_base'
 
-class Puppet::Application::Query < Puppet::Application::FaceBase
+class Puppet::Application::Puppetdbquery < Puppet::Application::FaceBase
   def self.setting
     use_ssl = true
     begin

commit 5fba007a9f0ad0d49ba536c773592fc8dcbb159a

Fix renaming CLI command to puppetdbquery
Fixes regression introduced in 2c744337432e5601114ad64e59c335ff25b0bc1d

Currently, following errors are printed:

* Error: Could not parse application options: cannot load such file -- puppet/application/query
* Error: uninitialized constant Puppet::Application::Query
* Error: Try 'puppet help puppetdbquery facts' for usage

Closes #106

lib/puppet/face/puppetdbquery.rb
@@ -1,4 +1,4 @@
-require 'puppet/application/query'
+require 'puppet/application/puppetdbquery'
 require 'puppet/face'
 require 'puppet/util/colors'
 
@@ -13,17 +13,17 @@ Puppet::Face.define(:puppetdbquery, '1.0.0') do
 
   option '--host PUPPETDB' do
     summary 'Host running PuppetDB. '
-    default_to { Puppet::Application::Query.setting[:host] }
+    default_to { Puppet::Application::Puppetdbquery.setting[:host] }
   end
 
   option '--port PORT' do
     summary 'Port PuppetDB is running on'
-    default_to { Puppet::Application::Query.setting[:port] }
+    default_to { Puppet::Application::Puppetdbquery.setting[:port] }
   end
 
   option '--no_ssl' do
     summary 'Talk plain HTTP instead of HTTPS'
-    default_to { !Puppet::Application::Query.setting[:use_ssl] }
+    default_to { !Puppet::Application::Puppetdbquery.setting[:use_ssl] }
   end
 
   action :facts do